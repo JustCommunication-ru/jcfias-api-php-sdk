8114dc55172de37cfb5836c395fdf720e676e180
worka <bckworka@gmail.com>
2021-06-07 15:46:06 +1000

    Update SearchRequest.php

--- src/API/SearchRequest.php
+++ src/API/SearchRequest.php
@@ -23,7 +23,7 @@ class SearchRequest extends AbstractRequest
     public function __construct($string, $params = [])
     {
         $this->options = [
-            'guid_path' => $params['guid_path'] ?? [],
+            'guid_path' => isset($params['guid_path']) ? $params['guid_path'] : [],
             'autocomplete' => isset($params['autocomplete']),
             'assumptions' => isset($params['assumptions'])
         ];
