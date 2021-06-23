e0f7e1630731a95fe70559540be1c143c5830ec9
worka <bckworka@gmail.com>
2021-06-23 11:12:47 +1000

    Update TownsRequest.php

--- src/API/TownsRequest.php
+++ src/API/TownsRequest.php
@@ -4,7 +4,7 @@ namespace JustCommunication\JcFIASSDK\API;
 
 class TownsRequest extends AbstractRequest
 {
-    const URI = '/api/towns';
+    const URI = '/towns';
     const HTTP_METHOD = 'GET';
     const RESPONSE_CLASS = TownsResponse::class;
 }
